96d8bd550d78fc0e5e869a2a9963f0d8b2e30b55
mmenuu <rctkhtp@gmail.com>
2022-11-19 17:54:59 +0700

    shift world

--- code/menu.py
+++ code/menu.py
@@ -70,7 +70,7 @@ def main_menu(running,start):
         screen.blit(BG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
-
+        
         MENU_TEXT = get_font(100).render("MEOW UP!", True, "White")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 250))
         
